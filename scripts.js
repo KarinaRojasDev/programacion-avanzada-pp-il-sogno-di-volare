54187caa116df54478a9d1177a43c8001dc7bdd6
Karina Rojas <karinacodecompetent@gmail.com>
2026-03-24 16:55:47 +0100

    Ejercicios completados, programación orientada a objetos

--- scripts.js
+++ scripts.js
@@ -32,35 +32,56 @@ const militaryHelicopter = [
 
 class Gallery {
     constructor(civilImages, militaryImages) {
+        this.civilImages = civilImages;
+        this.militaryImages = militaryImages;
     }
     
     getRandomCivil() {
+        const aleatorioVehiculoCivil = Math.floor(Math.random() * this.civilImages.length);
+        return this.civilImages[aleatorioVehiculoCivil];
     }
     
     getRandomMilitary() {
+        const aleatorioVehiculoMilitar = Math.floor(Math.random() * this.militaryImages.length);
+        return this.militaryImages[aleatorioVehiculoMilitar];
     }
     
     getAll() {
+        return [...this.civilImages, ...this.militaryImages];
     }
 }
 
 class Painter {
     constructor() {
+        this.createGallery();
     }
 
     createGallery() {
+        this.gallery = document.createElement("section");
+        document.body.appendChild(this.gallery);
     }
 
     createImageTag(imageUrl) {
+        let picture = document.createElement("picture");
+        let img = document.createElement("img");
+        img.src = imageUrl;
+        picture.appendChild(img);
+        return picture;
     }
 
     paintSingleImage(imageUrl) {
+        let picture = this.createImageTag(imageUrl);
+        this.gallery.appendChild(picture);
     }
 
     paintMultipleImages(arrayOfImages) {
+        arrayOfImages.forEach(img =>{
+            this.paintSingleImage(img);
+        })
     }
 }
 
 const aircrafts = new Gallery(civilAircrafts, militaryAircrafts);
 const helicopters = new Gallery(civilHelicopters, militaryHelicopter);
 const painter = new Painter();
+
